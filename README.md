9b301e568e06b68dfb1b7123abb9d9ae31a051b7
muell <me163@live.jp>
2016-09-08 03:07:49 +0900

    to use web browser instead of client.py

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # DisplayOff
-A Python script for turning off (on) the display by using Win32API.  
-Server and client programs are also included so that you may run this script from remote computer.
+A Python script for turning (on|off) the display by using Win32API.  
+Server program is also included so that you may run this script from the web browser on your remote computers.
 
+## main.py
+Turning (on|off) by local compter.
 ```
-usage: main.py [-h] [-x | -o] [-w WAIT] 
+usage: 
+python main.py [-h] [-x | -o] [-w WAIT] 
 
 optional arguments: 
   -h, --help            show this help message and exit 
@@ -11,3 +14,17 @@ optional arguments:
   -o, --on              Turn on the display.
   -w WAIT, --wait WAIT  wait <WAIT> seconds before turning off (on). (default=0)
   ```
+
+  ## server.py
+  
+```
+usage:
+python server.py
+
+open your browser and access  "http://host:port/(on|off)[?wait seconds]".
+for example:
+ "http://localhost:8000/off" means to turn off  immediately.
+ "http://192.168.0.2:8080/on?5" means to turn on in 5 seconds.
+
+ ```
+ to configure host and port, please see config.ini.
--- server.py
+++ server.py
@@ -2,21 +2,46 @@
 import xmlrpc.server as xmlrpc_server
 import configparser
 import dispctrl
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from urllib.parse import urlparse
+
+
+#serverShutdown=False # flag whether stop this server.
+
+config = configparser.SafeConfigParser()
+config.read("./config.ini")
+
+host = config.get('Server','Host')
+port = int(config.get('Server','Port'))
+
+class MyHandler(SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        url=urlparse(self.path)
+        body = b'Usage: http://host:port/(on|off)[?wait seconds]'
+        wait=0
+
+        if url.query.isdigit() and int(url.query) >0:
+            wait=int(url.query)
+        
+        if url.path == '/on':
+            dispctrl.DispOn(wait)
+            body = b'Turning on succeeded.'
+        elif url.path == '/off':
+            dispctrl.DispOff(wait)
+            body = b'Turning off succeeded.'
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.send_header('Content-length', len(body))
+        self.end_headers()
+        self.wfile.write(body)
+
 
 def main():
-    config = ConfigParser.SafeConfigParser()
-    config.read("./config.ini")
-
-    IPaddress = (config.get('Server','IP'), int(config.get('Server','Port')))
-
-    server = xmlrpc_server.SimpleXMLRPCServer(IPaddress)
-
-    server.register_function(dispctrl.DispOn,'DispOn') 
-    server.register_function(dispctrl.DispOff, 'DispOff')
-
-    server.register_introspection_functions()
-
-    server.serve_forever()
+    httpd = HTTPServer((host, port), MyHandler)
+    print('Server starting... press Ctrl+C to exit.')
+    httpd.serve_forever()
 
 if __name__ == '__main__':
     main()
