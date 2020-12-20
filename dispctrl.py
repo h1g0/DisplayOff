59f77930966c9aa3542fa7cfa1d87217ffd10a13
hi_go <me@clutte.red>
2020-12-20 18:32:56 +0000

    Add type annotations.

--- dispctrl.py
+++ dispctrl.py
@@ -1,27 +1,36 @@
 # -*- coding: utf-8 -*-
 from ctypes import windll
 from time import sleep
+from types import Final
 
-HWND_BROADCAST = 0xFFFF
-WM_SYSCOMMAND = 0x0112
-SC_MONITORPOWER = 0xF170
-DISPLAY_ON = -1
-DISPLAY_OFF = 2
+HWND_BROADCAST: Final[int] = 0xFFFF
+WM_SYSCOMMAND: Final[int] = 0x0112
+SC_MONITORPOWER: Final[int] = 0xF170
+DISPLAY_ON: Final[int] = -1
+DISPLAY_OFF: Final[int] = 2
 
-def DispOn(waitTime):
-    sleep(waitTime)
-    return windll.user32.PostMessageA(
+def DispOn(wait_time: int)->bool:
+    sleep(wait_time)
+    result = windll.user32.PostMessageA(
         HWND_BROADCAST,
         WM_SYSCOMMAND, 
         SC_MONITORPOWER,
         DISPLAY_ON
         )
+    if result is 0:
+        return False
+    else:
+        return True
 
-def DispOff(waitTime):
-    sleep(waitTime)
-    return windll.user32.PostMessageA(
+def DispOff(wait_time: int)->bool:
+    sleep(wait_time)
+    result = windll.user32.PostMessageA(
         HWND_BROADCAST,
         WM_SYSCOMMAND, 
         SC_MONITORPOWER,
         DISPLAY_OFF
         )
+    if result is 0:
+        return False
+    else:
+        return True
--- server.py
+++ server.py
@@ -4,46 +4,47 @@ import configparser
 import dispctrl
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from urllib.parse import urlparse
+from types import Final
 
-
-#serverShutdown=False # flag whether stop this server.
+# serverShutdown=False # flag whether stop this server.
 
 config = configparser.SafeConfigParser()
 config.read("./config.ini")
 
-host = config.get('Server','Host')
-port = int(config.get('Server','Port'))
+HOST: Final[str] = config.get('Server', 'Host')
+PORT: Final[int] = int(config.get('Server', 'Port'))
+
 
 class MyHandler(SimpleHTTPRequestHandler):
 
     def do_GET(self):
-        url=urlparse(self.path)
+        url = urlparse(self.path)
         body = bytes('''<h1>DisplayOff</h1><hr>
         Usage: http://{0}:{1}/(on|off)[?wait seconds]<br>
         <br>
         Quick link:<br>
         <a href=http://{0}:{1}/off>Turn off now</a><br>
         <a href=http://{0}:{1}/on>Turn on now</a><br>
-        '''.format(host,port),'ascii')
-        wait=0
+        '''.format(HOST, PORT), 'ascii')
+        wait = 0
 
-        if url.query.isdigit() and int(url.query) >0:
-            wait=int(url.query)
-        
+        if url.query.isdigit() and int(url.query) > 0:
+            wait = int(url.query)
+
         if url.path == '/on':
             dispctrl.DispOn(wait)
             body = bytes('''<h1>DisplayOff</h1><hr>
             Turning on succeeded.<br>
             <br>
             <a href=http://{0}:{1}/>Back to top page</a><br>
-            '''.format(host,port),'ascii')
+            '''.format(HOST, PORT), 'ascii')
         elif url.path == '/off':
             dispctrl.DispOff(wait)
             body = bytes('''<h1>DisplayOff</h1><hr>
             Turning off succeeded.<br>
             <br>
             <a href=http://{0}:{1}/>Back to top page</a><br>
-            '''.format(host,port),'ascii')
+            '''.format(HOST, PORT), 'ascii')
 
         self.send_response(200)
         self.send_header('Content-type', 'text/html; charset=utf-8')
@@ -53,9 +54,10 @@ class MyHandler(SimpleHTTPRequestHandler):
 
 
 def main():
-    httpd = HTTPServer((host, port), MyHandler)
-    print('Server starting... press Ctrl+C to exit.')
+    httpd = HTTPServer((HOST, PORT), MyHandler)
+    print('Server started... Press Ctrl+C to exit.')
     httpd.serve_forever()
+
 
 if __name__ == '__main__':
     main()
